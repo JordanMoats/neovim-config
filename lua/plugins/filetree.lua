eb9123a35c0dc670b154ae75de18116e8007a11b
Jordan Moats <jmoats@ggc.edu>
2024-11-06 22:37:53 -0500

    adds filetree

---
+++ lua/plugins/filetree.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("neo-tree").setup({
+                filesystem = {
+                    filtered_items = {
+                        visible = false,
+                        hide_dotfiles = false,
+                        hide_gitignored = false,
+                    },
+                },
+            })
+            -- Add keybind to toggle file tree
+            vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>')  -- Toggle file explorer
+        end
+    }
+}
